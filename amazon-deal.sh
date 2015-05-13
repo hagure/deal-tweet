c6e89526515d52776ecea7dcbbbd7fc24e0ee777
Terry McCall <junkhate@gmail.com>
2015-05-12 17:21:21 -0700

    Initial Sync Fixes

--- amazon-deal.sh
+++ amazon-deal.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+SERVER = "hm"
+PATH_PY_VIRTUAL = "~/path/to/server/bin/activate"
+PATH_PY_DEALS = "~/path/to/server/scripts/amazon-deal.py"
 
 #	Add local library checking
 
-ssh hm "source ~/hebi/bin/activate; python ~/hebi/scripts/amazon-deal.py $1"
+ssh hm "source '$PATH_PY_VIRTUAL'; python '$PATH_PY_DEALS' $1"
